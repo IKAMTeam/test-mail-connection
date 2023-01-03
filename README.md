0f14a36d9ed4c9d659a14b06ba233818ccc51603
eugene <evgenii.ivannikov@onevizion.com>
2023-01-03 10:48:20 +0500

    feat(DEPL-219948): Add a standalone app to this repository

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# test-mail-connection
-Simple standalone app to test connections to the mail server using OAuth2 authentication
+# mailbotvizion
---
+++ src/main/java/onevizion/mail/bot/BotApplication.java
@@ -0,0 +1,79 @@
+package onevizion.mail.bot;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Store;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class BotApplication {
+    private static final String MAIL_PROPERTIES_PATH = "/mail.properties";
+
+    public static void main(String[] args) throws IOException, MessagingException {
+        connect(args[0]);
+    }
+
+    private static void connect(String pathCredentials) throws MessagingException, IOException {
+        Properties credentials = getCredentials(pathCredentials);
+        Session session = Session.getInstance(getMailProperties());
+        Store store = session.getStore("imap");
+        store.connect(
+                credentials.getProperty("host"),
+                Integer.parseInt(credentials.getProperty("port")),
+                credentials.getProperty("username"),
+                getToken(credentials)
+        );
+    }
+
+    private static String getToken(Properties props) throws IOException {
+        HttpClient httpClient = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost(props.getProperty("token_uri"));
+
+        List<NameValuePair> params = new ArrayList<NameValuePair>(2);
+        params.add(new BasicNameValuePair("client_id", props.getProperty("client_id")));
+        params.add(new BasicNameValuePair("client_secret", props.getProperty("client_secret")));
+        params.add(new BasicNameValuePair("scope", props.getProperty("scope")));
+        params.add(new BasicNameValuePair("grant_type", props.getProperty("grant_type")));
+        httpPost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
+
+        HttpResponse response = httpClient.execute(httpPost);
+        String responseJson = EntityUtils.toString(response.getEntity());
+
+        JSONObject jsonObject = new JSONObject(responseJson);
+
+        return jsonObject.getString("access_token");
+    }
+
+    private static Properties getMailProperties() {
+        Properties prop = new Properties();
+        try (InputStream inputStream = BotApplication.class.getResourceAsStream(MAIL_PROPERTIES_PATH)) {
+            prop.load(inputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return prop;
+    }
+
+    private static Properties getCredentials(String path) throws IOException {
+        Properties prop = new Properties();
+        try (InputStream inputStream = new FileInputStream(path)) {
+            prop.load(inputStream);
+        }
+        return prop;
+    }
+
+}
